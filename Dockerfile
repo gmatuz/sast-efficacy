8d98097a42b008d375ab5fddce66d8f81688daae
Gabor Matuz <gabor.matuz@real-digital.de>
2023-07-23 00:44:24 -0400

    restructuring, cleanup and diffing

--- Dockerfile
+++
@@ -1,4 +0,0 @@
-FROM python
-RUN pip install -r requiremenets.txt
-RUN apt-get install -y git sed
-RUN git clone https://github.com/github/advisory-database /src
--- extract_sources.py
+++ extract_sources.py
@@ -2,6 +2,7 @@ import os
 import json
 import jsonschema
 import subprocess
+import datetime
 
 advisories = "https://github.com/github/advisory-database"
 
@@ -24,8 +25,12 @@ cwd = os.getcwd()
 # repo = git.Repo.clone_from(advisories, f"{cwd}/advisories" )
 
 min_schema = json.load(open("min_schema.json"))
+out_file = "enriched_vuln_sources.json"
 
-num_samples_to_resolve = 200
+debug = False
+debug_num_samples_to_resolve = 200
+
+update_frequency_days = 7
 
 
 def parse_advisory(advisory, results):
@@ -36,10 +41,13 @@ def parse_advisory(advisory, results):
             "language": language_map.get(affected["package"]["ecosystem"]) if affected["package"]["ecosystem"] in language_map else affected["package"]["ecosystem"],
             "vulnerable_version":  affected["ranges"][0]["events"][0]["introduced"],
             "fixed_version":  affected["ranges"][0]["events"][1]["fixed"],
-            "repo": next(filter(lambda ref: ref["type"] == "PACKAGE", advisory["references"])),
+            # Extracting the package type links and fixing up subpaths
+            "repo": next(filter(lambda ref: ref["type"] == "PACKAGE", advisory["references"]))["url"].partition('.git')[0].partition('/tree/')[0],
             "type": advisory["database_specific"]["cwe_ids"],
             "severity": advisory["database_specific"]["severity"],
-            "CVE_ID": advisory["aliases"]
+            "CVE_ID": advisory["aliases"],
+            "id": advisory["id"],
+            "modified": advisory["modified"]
         })
     except:
         # TODO check if any of these advisories are salvageable
@@ -47,7 +55,7 @@ def parse_advisory(advisory, results):
 
 
 def read_advisories(dir, results):
-    if len(results) > num_samples_to_resolve:
+    if len(results) > debug_num_samples_to_resolve and debug:
         return
     for name in os.listdir(dir):
         path = f"{dir}/{name}"
@@ -59,6 +67,7 @@ def read_advisories(dir, results):
 
 
 def get_tag_for_version(repository, version):
+    # Sometimes protocol is missing, sometimes it is http but that is recovered by the client
     if not repository.startswith("http"):
         repository = f"https://{repository}"
     # The last version that includes the version number to make sure it includes the fix already, compared to an RC
@@ -66,6 +75,7 @@ def get_tag_for_version(repository, version):
     p = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
     out = ""
     try:
+        # In some cases user interaction is expected to input user/pwd; hence the timeout
         (output, err) = p.communicate(timeout=10)
         out = output.decode().strip().split('/')[-1]
     except:
@@ -92,14 +102,14 @@ def get_tag_for_previous_version(repository, version):
 
 def enrich_git_tag(vuln):
     vuln["fixed_tag"] = get_tag_for_version(
-        vuln["repo"]["url"], vuln["fixed_version"])
+        vuln["repo"], vuln["fixed_version"])
     if vuln["vulnerable_version"] == "0":
         # If there is no specific version noted we take the version previous to the fix
         vuln["vulnerable_tag"] = get_tag_for_previous_version(
-            vuln["repo"]["url"], vuln["fixed_version"])
+            vuln["repo"], vuln["fixed_version"])
     else:
         vuln["vulnerable_tag"] = get_tag_for_version(
-            vuln["repo"]["url"], vuln["vulnerable_version"])
+            vuln["repo"], vuln["vulnerable_version"])
     if vuln["fixed_tag"] and vuln["vulnerable_tag"]:
         return vuln
     # TODO check if any of these advisories are salvageable
@@ -114,8 +124,26 @@ def create_repo_db():
     read_advisories(
         f"{cwd}/advisory-database/advisories/github-reviewed", vulns)
     vulns = list(filter(lambda vuln: vuln, map(enrich_git_tag, vulns)))
-    with open("enriched_vuln_sources.json", "w") as f:
-        json.dump(vulns, f)
+    with open(out_file, "w") as f:
+        json.dump(vulns, f, indent=4)
 
 
-create_repo_db()
+def update_repo_db():
+    update_since = datetime.datetime.now() - datetime.timedelta(days=update_frequency_days)
+    vulns = []
+    # read_advisories(f"{cwd}/advisories/", vulns)
+    # actually I don't think others will include any versions
+    # TODO check if any of these advisories are salvageable
+    read_advisories(
+        f"{cwd}/advisory-database/advisories/github-reviewed", vulns)
+    vulns = list(filter(lambda vuln: vuln,
+                        map(enrich_git_tag,
+                            list(filter(lambda vuln: datetime.datetime.strptime(vuln["modified"], '%Y-%m-%dT%H:%M:%SZ') > update_since, vulns)))))
+    if vulns:
+        print(vulns)
+        vulns_old = json.load(open(out_file))
+        print(map(lambda update_vuln: update_vuln["id"], vulns))
+        vulns = vulns + list(filter(lambda vuln: vuln["id"] not in map(
+            lambda update_vuln: update_vuln["id"], vulns), vulns_old))
+        with open(out_file, "w") as f:
+            json.dump(vulns, f, indent=4)
